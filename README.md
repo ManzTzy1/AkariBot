a1af54639ada2f3b96e1752338dc0a2398c7e881
ManzTzy1 <aurelcangtip07@gmail.com>
2022-05-30 12:53:20 +0700

    Akari Bot 🎭

---
+++ README.md
@@ -0,0 +1,83 @@
+# CATATAN ( NOTE )
+Base Ori Dika Ardnt
+
+Recode Manz Tzy
+
+## NOTE
+This Script is for everyone, not for Sale. Jika dijual neraka menunggumu brother !
+
+This is Script of WhatsApp multi device, working with [`@adiwajshing/baileys`](https://github.com/adiwajshing/baileys)
+
+
+## UNTUK PENGGUNA WINDOWS/RDP
+
+* Unduh & Instal Git [`Klik Disini`](https://git-scm.com/downloads)
+* Unduh & Instal NodeJS [`Klik Disini`](https://nodejs.org/en/download)
+* Unduh & Instal FFmpeg [`Klik Disini`](https://ffmpeg.org/download.html) (**Jangan Lupa Tambahkan FFmpeg ke variabel lingkungan PATH**)
+
+
+```bash
+git clone https://github.com/manzkun/akari
+cd akari
+npm install
+```
+
+## HOW TO CONNECT TO MONGODB WHEN RUN IN HEROKU
+
+* Create account and database in mongodb atlas [`watch here`](https://youtu.be/rPqRyYJmx2g)
+* when you already have a database, you just need to take mongourl
+* Put mongourl in Procfile `web: node . --db 'mongourl'`
+* Example `web: node . -- db 'Your Mongo URI'`
+
+
+
+## FOR TERMUX/UBUNTU/SSH USER
+
+```bash
+apt update && apt upgrade
+apt install git -y
+apt install nodejs -y
+apt install ffmpeg -y
+git clone https://github.com/DikaArdnt/Hisoka-Morou
+cd Hisoka-Morou
+npm install
+```
+
+## RECOMMENDED INSTALL ON TERMUX
+
+```bash
+pkg install yarn
+yarn
+```
+
+## Installing
+```bash
+$ node .
+```
+
+## ❗ Warning
+WhatsApp bot is still in the development stage, so there are a few bugs
+WhatsApp Connection (BETA, not working perfectly)
+
+Editing Number Owner & session name in [`config.js`](https://github.com/manzkun/akari/blob/master/setting.js)
+Get Apikey zenz on [`zenz`](https://zenzapi.xyz/pricing)
+
+
+## Thanks To
+* [`@adiwajshing/baileys`](https://github.com/adiwajshing/baileys)
+* [`Nurutomo`](https://github.com/Nurutomo)
+* [`Mhankbarbar`](https://github.com/MhankBarBar)
+* [`Faiz`](https://github.com/FaizBastomi)
+* [`Gimenz`](https://github.com/Gimenz)
+* [`rayy`](https://github.com/rayyreall)
+* [`Fatih Arridho`](https://github.com/FatihArridho)
+* [`zhwzein`](https://github.com/zhwzein)
+* [`CAF-ID`](https://github.com/CAF-ID)
+* [`bintang`](https://github.com/Bintangp02)
+* [`ManzTzy`](https://github.com/ManzTzy1)
+* [`manzkun`](https://github.com/manzkun)
+
+```Thanks to all who have participated in the development of this script```
+
+
+License: [MIT](https://en.wikipedia.org/wiki/MIT_License)
---
+++ setting.js
@@ -0,0 +1,113 @@
+/**
+   * Base By Dika Ardnt.
+   * Recode Manz Tzy
+   * Wa Me https://wa.me/6281327496283
+   * Nama session nya manztzy.json
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Ga usah di ubah" dah free apikey
+global.APIs = {
+	web: 'https://revita.herokuapp.com',
+	free: 'https://api-invibot.herokuapp.com',
+	lol: 'https://api.lolhuman.xyz',
+}
+
+// Ga usah di ubah" dah free apikey
+global.APIKeys = {
+	'https://revita.herokuapp.com': 'ApiRevita',
+	'https://api-invibot.herokuapp.com': 'APIKEY',
+	'https://api.lolhuman.xyz': 'Atakbotz',
+}
+
+// Other
+global.ownerr = ['6281327496283']
+global.premium = ['6288292024190']
+global.owner = ['6281327496283','6283846819169']
+global.packname = 'Hisoka Morrow'
+global.author = 'WhatsApp Bot'
+global.bot = '𝐴𝑘𝑎𝑟𝑖 𝐵𝑜𝑡⃪᭪'
+global.rev = 'ApiRevita'
+global.lolkey = 'Atakbotz'
+global.invi = 'APIKEY'
+global.zen = 'BagasPrdn'
+global.ownmy = '𝑴𝒂𝒏𝒛 𝑻𝒛𝒚'
+global.sessionName = 'manz'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.bisa = 'Nih Om 々'
+global.sp = '⭔'
+global.copy = '𝑴𝒂𝒏𝒛 𝑻𝒛𝒚'
+global.punya = 'Punya'
+global.wet = '*「 WAIT DONT SPAM 」*'
+global.man = {
+kus: '*「 FITUR KHUSUS OWNER 」*',
+dir: '*「 HANYA BISA DI GUNAKAN DI PRIVATE CHAT 」*',
+wet: '*「 WAIT DONT SPAM 」*',
+bomin: '*「 BOT HARUS JADI ADMIN 」*',
+gcwa: '*「 GROUP ONLY JOIN HERE 」: https://chat.whatsapp.com/DSFTbb0giUsH9borzEDGEv*',
+ami: '*「 KHUSUS ADMIN GC 」*',
+}
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 1,
+}
+global.thumb = fs.readFileSync('./lib/man.jpg')
+global.visoka = fs.readFileSync('./lib/manz.mp4')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
+
+//cuma pajangan 🗿
+global.terr = `Ini Adalah Bot Baileys Multi Device, Dan Gua Juga Masih Pemula 😅 Jadi Wajar Kalo Tampilan Nya Jelek Dan Masih Ada Yang Eror`
+global.sn = `Jangan Sange Ama Gambar Ya 🗿`
+
+
+//menu bot
+global.menu = `apa sih`
+global.ter = `generate random image`
+
+//donasi
+global.nasi = `╭───⌜ *Donasi 💌* ⌟──❑
+│▸ *Mau Donasi Apa Liat Doang Bang ?*
+│▸ Eh gua ga punya apk dana/gopay dll :v
+│▸ kalo pulsa boleh lah buat beli kuota 🗿
+│▸ pulsa: 0895397918020
+╰───⌜ *${bot}*⌟─❑
+
+╭───⌜ *Sewa Bot 💌* ⌟──❑
+│▸ list Harga =
+│▸ permanen = 7k
+│▸ keuntungan: 
+│▸ bot bisa di taruh di gc lu
+│▸ bisa menggunakan semua fitur-fitur bot
+╰───⌜ *${bot}* ⌟─❑`
+
+
+//script
+
+global.sc = `╭───⌜ *Script* ⌟──❑
+│▸ *Base Ori By Dika Ardnt*
+│▸ github: https://www.github.com/DikaArdnt/Hisoka-Morou
+│▸ *Recode By Manz Tzy*
+│▸ sc: https://www.github.com/manzkun/akari
+╰───⌜ *${bot}* ⌟─❑`
+
+global.sdkm = '```Mohon Di Patuhi Peraturan Ini Terma Kasih 🙏```'
